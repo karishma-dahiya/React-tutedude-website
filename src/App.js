31a7d6772e2fadf121f998c1895684ae0afccc51
karishma <88223924+karishma-dahiya@users.noreply.github.com>
2023-05-24 21:08:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Friends from './pages/Friends';
 
